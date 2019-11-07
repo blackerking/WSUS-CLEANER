8bccebf47eef2f23732d54500d438cf723f2f9cb
blackerking <2019363+blackerking@users.noreply.github.com>
2019-11-07 13:27:58 +0100

    small update
    * added titel for every work
    * small typo fixes

--- WSUS Cleanup.ps1
+++ WSUS Cleanup.ps1
@@ -13,32 +13,53 @@ $TrialRun = 0
 # 1 = Yes
 # 0 = No#
 
+
+
 #Ermittle Sprache
 [String]$Sprache = [CultureInfo]::InstalledUICulture | select TwoLetter* 
 $Sprache = $Sprache.Remove(0,27)
 $Sprache= $Sprache.Replace("}",$null)
 
-    echo "Englische Text ausgabe " "`0"
-    $IA64_text =" Itanium updates were declined"
-    $ARM64_text =" ARM64 Updates were declined"
-    $embedded_text =" Windows Embedded Updates were declined"
-    $Office64_text =" MS Office 64-Bit were declined"
-    $LanguagePack_text =" Language Interface Packs were declined"
-    $Start_Name =" WSUS cleaning started"
+
 
 ####Sprachtexte anpassen
 if($Sprache -eq "de")
 {
-    echo "Deutsche Text ausgabe " "`0"
+    #echo "Deutsche Text ausgabe " "`0"
+
     $IA64_text =" Itanium updates wurden abgelehnt"
     $ARM64_text =" ARM64 Updates wurden abgelehnt"
     $embedded_text =" Windows Embedded Updates wurden abgelehnt"
     $Office64_text =" MS Office 64-Bit wurden abgelehnt"
     $LanguagePack_text =" Language Interface Packs / Sprachpakete wurden abgelehnt"
-    $Start_Name =" WSUS Reinigung gestartet"
+    $Start_Name ="WSUS Reinigung gestartet"
+    
+    $IA64_titel ="Itanium updates werden abgelehnt"
+    $ARM64_titel ="ARM64 Updates werden abgelehnt"
+    $embedded_titel ="Windows Embedded Updates werden abgelehnt"
+    $Office64_titel ="MS Office 64-Bit werden abgelehnt"
+    $LanguagePack_titel ="Language Interface Packs / Sprachpakete werden abgelehnt"
+    $Cleaning_titel = "Reinigung gestartet"
 }
+else
+{ 
+    #echo "Englische Text ausgabe " "`0"
 
-echo "$Start_Name"
+    $IA64_text =" Itanium updates were declined"
+    $ARM64_text =" ARM64 Updates were declined"
+    $embedded_text =" Windows Embedded Updates were declined"
+    $Office64_text =" MS Office 64-Bit were declined"
+    $LanguagePack_text =" Language Interface Packs were declined"
+    $Start_Name ="WSUS cleaning started"
+
+    $IA64_titel ="Itanium updates werden abgelehnt"
+    $ARM64_titel ="ARM64 Updates werden abgelehnt"
+    $embedded_titel ="Windows Embedded Updates werden abgelehnt"
+    $Office64_titel ="MS Office 64-Bit werden abgelehnt"
+    $LanguagePack_titel ="Language Interface Packs / Sprachpakete werden abgelehnt"
+    $Cleaning_titel = "Reinigung gestartet"
+}
+echo "################## $Start_Name ##################"
 
 # Connect to the WSUS 3.0 interface.
 [reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") | out-null
@@ -48,7 +69,8 @@ $WsusServerAdminProxy = [Microsoft.UpdateServices.Administration.AdminProxy]::Ge
 # Suche Updates nach Namen und lehne diese ab
 
 # Itanium/IA64
-$itanium = $WsusServerAdminProxy.GetUpdates() | ?{-not $_.IsDeclined -and $_.Title -match “ia64|itanium”}
+echo "$IA64_titel"
+$itanium = $WsusServerAdminProxy.GetUpdates() | ?{-not $_.IsDeclined -and $_.Title -match "ia64|itanium"}
 $IA64_counted = $itanium.count
     If ($itanium.count -lt 1)
         {
@@ -61,9 +83,10 @@ $IA64_counted = $itanium.count
 
 echo "$IA64_counted $IA64_text"
 
+echo "$ARM64_titel"
 
 # ARM64
-$arm64 = $WsusServerAdminProxy.GetUpdates() | ?{-not $_.IsDeclined -and $_.Title -match “ARM64”}
+$arm64 = $WsusServerAdminProxy.GetUpdates() | ?{-not $_.IsDeclined -and $_.Title -match "ARM64"}
 $ARM64_counted = $arm64.count
     If ($ARM64.count -lt 1)
         {
@@ -75,10 +98,10 @@ $ARM64_counted = $arm64.count
         }
 
 echo "$ARM64_counted $ARM64_text"
-
+echo "$embedded_titel"
 
 # Windows Embedded
-$embedded = $WsusServerAdminProxy.GetUpdates() | ?{-not $_.IsDeclined -and $_.Title -match “Windows Embedded Standard”}
+$embedded = $WsusServerAdminProxy.GetUpdates() | ?{-not $_.IsDeclined -and $_.Title -match "Windows Embedded Standard"}
 $embedded_counted = $embedded.count
     If ($embedded.count -lt 1)
         {
@@ -90,10 +113,10 @@ $embedded_counted = $embedded.count
         }
 
 echo "$embedded_counted $embedded_text"
-
+echo "$Office64_titel"
 
 # MS Office 64-Bit
-$Office64 = $WsusServerAdminProxy.GetUpdates() | ?{-not $_.IsDeclined -and $_.Title -match “Excel|Lync|Office|Outlook|Powerpoint|Visio|word” -and $_.Title -match "64-bit"}
+$Office64 = $WsusServerAdminProxy.GetUpdates() | ?{-not $_.IsDeclined -and $_.Title -match "Excel|Lync|Office|Outlook|Powerpoint|Visio|word" -and $_.Title -match "64-bit"}
 $Office64_count = $Office64.count
     If ($Office64.count -lt 1)
         {
@@ -105,10 +128,10 @@ $Office64_count = $Office64.count
         }
 
 echo "$Office64_count $Office64_text"
-
+echo "$LanguagePack_titel"
 
 # Language Pack
-$LanguagePack = $WsusServerAdminProxy.GetUpdates() | ?{-not $_.IsDeclined -and $_.Title -match “Language Interface Pack|LanguageInterfacePack|LanguageFeatureOnDemand|Sprachpaket für|Language Pack - Windows 10 -|LanguagePack - Windows 10 Insider Preview|Lan Pack (Language Features)”}
+$LanguagePack = $WsusServerAdminProxy.GetUpdates() | ?{-not $_.IsDeclined -and $_.Title -match "Language Interface Pack|LanguageInterfacePack|LanguageFeatureOnDemand|Sprachpaket f|Language Pack - Windows 10 -|LanguagePack - Windows 10 Insider Preview|Lan Pack (Language Features)"}
 $LanguagePack_counted = $LanguagePack.count
     If ($LanguagePack.count -lt 1)
         {
@@ -120,6 +143,6 @@ $LanguagePack_counted = $LanguagePack.count
         }
 
 echo "$LanguagePack_counted $LanguagePack_text"
-
+echo "$Cleaning_titel"
 
 Invoke-WsusServerCleanup -UpdateServer $WSUS -DeclineExpiredUpdates -DeclineSupersededUpdates -CleanupObsoleteUpdates -CleanupUnneededContentFiles -CompressUpdates
